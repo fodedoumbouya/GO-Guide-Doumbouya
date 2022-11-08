aec9c7b6c8ca43a3c5146ac1b72178780a7d428c
fodedoumbouya <doumbouyaf.fode@gmail.com>
2022-11-08 20:22:33 +0100

    adding channel tuto

---
+++ channel/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// We are creating our Structure
+type Person struct {
+	Name string
+	Age  int
+	Sexe string
+}
+
+// this Function only help us to put value in the channek
+func SendToChannel(channel chan Person, person Person) {
+	channel <- person
+}
+
+func main() {
+
+	// Creating  empty channel
+	channel := make(chan Person)
+	// creating our Person
+	p := Person{
+		Name: "name",
+		Age:  20,
+		Sexe: "Female",
+	}
+
+	// calling our function `SendToChannel` by passing our channel and the personne
+	// but we are executing it in goroutine that's why we have 'go' before our function
+	go SendToChannel(channel, p)
+
+	// we are saying here to wait for the out and then give the name to our varible 'name'
+	name := (<-channel).Name
+	// Display the name
+	fmt.Println(name)
+
+}
